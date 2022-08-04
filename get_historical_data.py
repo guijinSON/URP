fcad35ba39764206f228277fa15d7462395569d2
GUIJIN SON <spthsrbwls123@yonsei.ac.kr>
2022-08-04 20:08:26 +0900

    Update get_historical_data.py

--- get_historical_data.py
+++ get_historical_data.py
@@ -1,19 +1,18 @@
 import yfinance as yf
 import pandas as pd
-equity = pd.read_excel('(N=30)Equity List-GSON.xlsx')
 
-data = yf.download( 
-        tickers = ' '.join(equity['Symbols'].values),
-        start='2010-01-01',
-        end='2021-12-31',
-        # fetch data by interval (including intraday if period < 60 days)
-        # valid intervals: 1m,2m,5m,15m,30m,60m,90m,1h,1d,5d,1wk,1mo,3mo
-        interval = "1d",
-        group_by = 'ticker',
-        auto_adjust = True,
-        prepost = True,
-        threads = True,
-        proxy = None
-    )
-
-data.to_excel('(N=30 | Freq=1D) Historical Data-GSON.xlsx')
+def import_stock_historical(ticker):
+    data = yf.download( 
+            tickers = ticker, #' '.join(equity['Symbols'].values),
+            start='2010-01-01',
+            end='2021-12-31',
+            # fetch data by interval (including intraday if period < 60 days)
+            # valid intervals: 1m,2m,5m,15m,30m,60m,90m,1h,1d,5d,1wk,1mo,3mo
+            interval = "1d",
+            group_by = 'ticker',
+            auto_adjust = True,
+            prepost = True,
+            threads = True,
+            proxy = None
+        )
+    return data

8e4971aaaba86090d81853afb7169fd595abec50
GUIJIN SON <spthsrbwls123@yonsei.ac.kr>
2022-08-04 21:30:30 +0900

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,14 @@
+def log_output_distribution(counter):
+    try: dis_0  = counter[0]
+    except: dis_0 = 0
+
+    try: dis_1  = counter[1]
+    except: dis_1 = 0
+
+    try: dis_2  = counter[2]
+    except: dis_2 = 0
+
+    try: dis_3  = counter[3]
+    except: dis_3 = 0
+
+    return dis_0, dis_1, dis_2, dis_3

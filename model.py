b900fd8794e1501446ec8c6d8ef0644492acf8b8
GUIJIN SON <spthsrbwls123@yonsei.ac.kr>
2022-08-04 20:04:54 +0900

    Update model.py

--- model.py
+++ model.py
@@ -27,3 +27,22 @@ class MLP_TS(nn.Module):
     def forward(self,x):
         x = self.model(x)
         return x
+    
+class MLP_TS_with_Head(nn.Module):
+    def __init__(self,d_ffn,seq_len,num_layers):
+        super(MLP_TS_with_Head,self).__init__()
+        self.model = nn.Sequential(*[MLP_Block(d_ffn,seq_len) for _ in range(num_layers)])
+
+        #self.p_enc_1d_model_sum = Summer(PositionalEncoding1D(seq_len))
+
+        self.MLP_LAYER_1 = nn.Linear(seq_len,1)
+        self.MLP_LAYER_2 = nn.Linear(d_ffn,4)
+
+    def forward(self,x):
+        x = self.p_enc_1d_model_sum(x)
+        x = self.model(x)
+        x2 = self.MLP_LAYER_1(x)
+        x3 = torch.permute(x2,(0,2,1))
+        x4 = self.MLP_LAYER_2(x3)
+        
+        return x4

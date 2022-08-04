4bb718c29444d99768f245a8f12097c952e93a96
GUIJIN SON <spthsrbwls123@yonsei.ac.kr>
2022-08-04 21:34:33 +0900

    Update train.py

--- train.py
+++ train.py
@@ -2,7 +2,7 @@ import torch
 import wandb
 import numpy as np
 from collections import Counter
-from utils import log_output_distribution
+#from utils import log_output_distribution
 
 def single_epoch_train(model, optimizer, trainloader, loss_func, epoch, batch_size, device, wb=True):
     running_loss = 0.0 
@@ -34,3 +34,18 @@ def single_epoch_train(model, optimizer, trainloader, loss_func, epoch, batch_si
         wandb.log({'Training Accuracy':acc, 'Class 0': d0, 'Class 1': d1, 'Class2': d2, 'Class 3':d3})
     else:
         print('[%d] Running Accuracy: %.2f' % (epoch + 1, acc)) 
+
+def log_output_distribution(counter):
+    try: dis_0  = counter[0]
+    except: dis_0 = 0
+
+    try: dis_1  = counter[1]
+    except: dis_1 = 0
+
+    try: dis_2  = counter[2]
+    except: dis_2 = 0
+
+    try: dis_3  = counter[3]
+    except: dis_3 = 0
+
+    return dis_0, dis_1, dis_2, dis_3

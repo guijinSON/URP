e87389a295358cde6e709fa7132ce5da5c5bda44
GUIJIN SON <spthsrbwls123@yonsei.ac.kr>
2022-08-10 16:04:30 +0900

    Update data.py

--- data.py
+++ data.py
@@ -1,11 +1,11 @@
 import yfinance as yf
 import pandas as pd
 
-def import_stock_historical(ticker):
+def import_stock_historical(ticker,START='2010-01-01',END=2021-12-31'):
     data = yf.download( 
             tickers = ticker, #' '.join(equity['Symbols'].values),
-            start='2010-01-01',
-            end='2021-12-31',
+            start=START,
+            end=END,
             # fetch data by interval (including intraday if period < 60 days)
             # valid intervals: 1m,2m,5m,15m,30m,60m,90m,1h,1d,5d,1wk,1mo,3mo
             interval = "1d",
